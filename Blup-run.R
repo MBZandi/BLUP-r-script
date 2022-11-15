32dbaed94e74dd6fad724fa4acdc26f84144e5b5
Mohammad Bagher Zandi <48085022+MBZandi@users.noreply.github.com>
2022-11-15 21:52:53 +0330

    Update Blup-run.R

--- Blup-run.R
+++ Blup-run.R
@@ -1,11 +1,11 @@
-##### clean the memory
+##### Clean the memory
 
 rm(list=ls())
 
-#### open your script
+#### Open your script
 source("BLUP1.exe")
 
 
-### defile the prior of MME ==>> alpha=(varE, varG)
+### Defile the prior of MME ==>> alpha=(varE, varG)
 sol=alpha(100,200)
 
